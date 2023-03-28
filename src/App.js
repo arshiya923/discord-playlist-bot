d72570c694348f8b54d76ff0127544b8be34d389
Arshiya <gh.arshiya92@gmail.com>
2023-03-28 04:48:37 +0430

    Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import MainContent from './Components/MainContent/MainContent'
+import Footer from './Components/Footer/Footer'
 
 import './App.css'
 
@@ -9,6 +10,7 @@ const App = () => {
 		<div>
 			<Navbar />
 			<MainContent />
+			<Footer />
 		</div>
 	)
 }
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import './Footer.css'
+
 const Footer = () => {
 	return(
-		<div>
+		<div id='footer'>
 			<p>This Website Was Created By <span><a href='https://github.com/arshiya923'>Arshiya Gheibi</a></span></p>
 		</div>
 	)
